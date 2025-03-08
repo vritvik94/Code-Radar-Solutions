2632e7c9b9c1dd633fc347f7b1495f59d15bc286
Ritvik Verma <vritvik94@gmail.com>
2025-03-09 00:23:59 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,15 +2,9 @@
 
 int fibonacciSeries(f)
 {
-    int a=0,b=1,c,i;
-    if(f==0) return 0;
-    if(f==1) return 1;
-
-    for(i=2;i<=f;i++)
-    {
-        c=a+b;
-        a=b;
-        b=c;
-    }
-    return b;
+   int a=0,b=1,c;
+   c=a+b;
+   a=b;
+   b=c;
+   return b;
 }
